da46a973b2b1fa1d2c14c974b64e7c6b51c8e6d7
lux <xiao.lu@aihuishou.com>
2021-12-15 17:28:56 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType<KotlinCompile> {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "com.ahs.camera.ui.MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Exe, TargetFormat.Deb)
             packageName = "camera-desktop"
